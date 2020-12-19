3a9fe6c3f4d9c635b21acaa02f27cb411de48750
HarshSharma009 <47885066+HarshSharma009@users.noreply.github.com>
2020-12-19 23:36:04 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('sample.db')
-c = conn.cursor()
-#c.execute('create table students(name text,rno int)')
-c.execute("insert into students values('varaprasad',24)")
-conn.commit()
-conn.close()
